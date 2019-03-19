70c64c979b7585d32deaaba154770479fea16755
Supanccy <chenchaoyang2013@163.com>
2019-03-19 23:59:08 +0800

    优化代码，增加新闻详情页面

--- src/components/news/News-Recoment.vue
+++ src/components/news/News-Recoment.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="news-recoment">
-     推荐
+     <ul>
+
+       <li v-for="news in newsList">
+         <div class="title">{{news.title}}</div>
+         <div class="info">
+           <span v-show="news.isUp" class="up">置顶</span>
+           <span class="source">{{news.source}}</span>
+           <span class="comment">{{news.comment}}评论</span>
+           <span class="time">{{news.time}}</span>
+         </div>
+       </li>
+
+     </ul>
   </div>
 </template>
 
@@ -9,6 +21,67 @@ export default {
   name: 'NewsRecoment',
   data() {
     return {
+      newsList:[
+        {
+          title:'从一夜爆红到被疯狂质疑，香港“最美搬运工”港版罗拉后来怎么样了？',
+          source:'人民日报微信',
+          comment:355,
+          time:'1分钟前',
+          isUp:true,
+        },
+        {
+          title:'中国自美进口农产品将扩大到当前的3倍？中方回应',
+          source:'中国经济网',
+          comment:9652,
+          time:'3分钟前',
+          isUp:true,
+        },
+        {
+          title:'小偷日记写满“励志语录”警察:没喝过这么多鸡汤',
+          source:'中国新闻社',
+          comment:12302,
+          time:'20分钟前',
+          isUp:false,
+        },
+
+
+
+        {
+          title:'小米集团第四季度营收444亿元人民币，全年净利润136亿元人民币',
+          source:'AI财经社',
+          comment:3333,
+          time:'276分钟前',
+          isUp:false,
+        },
+        {
+          title:'中国首位芯片女掌门：打破国外芯片垄断！地位比肩华为任正非',
+          source:'正商参阅',
+          comment:2222,
+          time:'30分钟前',
+          isUp:false,
+        },
+        {
+          title:'没想到华为隐藏这么深，这次创下一世界纪录，超美国两巨头之和',
+          source:'中国科技先生',
+          comment:300,
+          time:'10小时前',
+          isUp:false,
+        },
+        {
+          title:'5G电话卡诞生了，普通用户4G过渡到5G到底是换手机还是SIM卡？',
+          source:'中国新闻社',
+          comment:12302,
+          time:'20分钟前',
+          isUp:false,
+        },
+        {
+          title:'【朝花夕拾】写给玩家的散文诗——《艾迪芬奇的记忆》简评与随感',
+          source:'艾渴echo',
+          comment:600,
+          time:'20分钟前',
+          isUp:false,
+        },
+      ],
     };
   },
   methods: {
@@ -17,5 +90,45 @@ export default {
 </script>
 
 <style scoped lang="less">
+  .news-recoment{
+    >ul{
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      >li{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        padding: 6px 10px;
+        .title{
+          height: 60px;
+          font-size: 16px;
+          font-weight: bold;
+          line-height: 30px;
+        }
+        .info{
+          height: 30px;
+          line-height: 30px;
+          color: #999999;
+          font-size: 12px;
+          span{
+            display: inline-block;
+            margin-left: 5px;
+            &:first-child{
+              margin-left: 0;
+              line-height: 1.3;
+              border-radius: 3px;
+            }
+          }
+          .up{
+            border: 1px solid #F04C4D;
+            color:#F04C4D;
+          }
+        }
+        border-bottom: 1px solid #E8E8E8;
+        margin-bottom: 10px;
+      }
+    }
+  }
 
 </style>
--- src/components/news/News.vue
+++ src/components/news/News.vue
@@ -2,11 +2,11 @@
   <div class="news">
      <div class="menu">
        <ul>
-         <li @click="switcSubMenu('/news/newsRecoment')">推荐</li>
-         <li @click="switcSubMenu('/news/newsIndex')">首页</li>
-         <li @click="switcSubMenu('/news/newsDomestic')">国内</li>
-         <li @click="switcSubMenu('/news/newsIndex')">国外</li>
-         <li @click="switcSubMenu('/news/newsIndex')">历史</li>
+         <li :class="newsSubMenuSelectNo=='1'?'newsSubMenuActive':''" @click="switcSubMenu('/news/newsRecoment',1)">推荐</li>
+         <li :class="newsSubMenuSelectNo=='2'?'newsSubMenuActive':''" @click="switcSubMenu('/news/newsIndex',2)">首页</li>
+         <li :class="newsSubMenuSelectNo=='3'?'newsSubMenuActive':''" @click="switcSubMenu('/news/newsDomestic',3)">国内</li>
+         <li :class="newsSubMenuSelectNo=='4'?'newsSubMenuActive':''" @click="switcSubMenu('/news/newsIndex',4)">国外</li>
+         <li :class="newsSubMenuSelectNo=='5'?'newsSubMenuActive':''" @click="switcSubMenu('/news/newsIndex',5)">历史</li>
        </ul>
      </div>
      <div class="content">
@@ -18,12 +18,15 @@
 <script>
 export default {
   name: 'News',
+  newsSubMenuSelectNo: 1,
   data() {
     return {
+      newsSubMenuSelectNo: 1,
     };
   },
   methods: {
-    switcSubMenu(path){
+    switcSubMenu(path,flag){
+      this.newsSubMenuSelectNo = flag;
       this.$router.push({path:path,query:{}});
     },
   },
@@ -32,12 +35,12 @@ export default {
 
 <style scoped lang="less">
   .news{
-    font-size: 14px;
+    font-size: 16px;
     height: 100%;
     padding-top: 40px;
     .menu{
       margin-top: -40px;
-      font-size: 14px;
+      font-size: 16px;
       height: 40px;
       line-height: 40px;
       background: #F5F7F9;
@@ -56,10 +59,16 @@ export default {
           padding: 0 15px;
           display: inline-block;
         }
+        .newsSubMenuActive{
+          color:#F04C4D;
+          font-weight: bold;
+        }
       }
     }
     .content{
       padding: 0.2rem 0.2rem;
+      height: 100%;
+      overflow: auto;
     }
   }
 </style>
